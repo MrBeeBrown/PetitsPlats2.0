346211241e933834761a29eb15bf3f7cfb11e937
beebrown <bruno.grosdesirs@outlook.fr>
2023-07-03 16:26:07 +0200

    maj affichage fiche recette

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 import { Recette } from "./recette.js";
 
+//Affichage du nombre de recettes
+const nbrRecettes = document.querySelector(".nbr_recettes");
+if (recipes.length <= 1) {
+  nbrRecettes.innerText = recipes.length + " " + "recette";
+} else {
+  nbrRecettes.innerText = recipes.length + " " + "recettes";
+}
+
+
+//Affichage des recettes
 recipes.forEach(element => {
   const ficheRecette = new Recette(element);
   ficheRecette.print();
+  console.log(recipes);
 });
--- js/recette.js
+++ js/recette.js
@@ -20,11 +20,43 @@ export class Recette {
     <img src="/assets/images/${this.image}" alt="${this.name}">
     <div class="timer">${this.time}min</div>
     <div class="recette_content">
-    <p class="titre_recette">${this.name}</p>
-    <p class="recette">Recette</p>    
-    <p class="description_recette">${this.description}</p>
-    <p class="liste_ingredients">Ingrédients</p>
+      <p class="titre_recette">${this.name}</p>
+      <p class="recette">Recette</p>    
+      <p class="description_recette">${this.description}</p>
+      <p class="liste_ingredients">Ingrédients</p>
+      <div class="ingredients">${this.printIngredients()}</div>
     </div>`;
     content.appendChild(recette);
-  };
+  }
+
+  printIngredients() {
+    //Affichage des ingrédients
+    const container = document.createElement("div");
+    for (let i = 0; i < this.ingredients.length; i++) {
+      if ((!this.ingredients[i].quantity) && (!this.ingredients[i].unit)) {
+        container.innerHTML += `
+        <div class="bloc_ingredient">
+          <p class="nom_ingredient">${this.ingredients[i].ingredient}</p>
+        </div>
+        `;
+      }
+      if ((this.ingredients[i].ingredient) && (this.ingredients[i].quantity) && (!this.ingredients[i].unit)) {
+        container.innerHTML += `
+        <div class="bloc_ingredient">
+          <p class="nom_ingredient">${this.ingredients[i].ingredient}</p>
+          <p class="nbr_ingredient">${this.ingredients[i].quantity}</p>
+        </div>
+        `;
+      }
+      if ((this.ingredients[i].ingredient) && (this.ingredients[i].quantity) && (this.ingredients[i].unit)) {
+        container.innerHTML += `
+        <div class="bloc_ingredient">
+          <p class="nom_ingredient">${this.ingredients[i].ingredient}</p>
+          <p class="nbr_ingredient">${this.ingredients[i].quantity}${this.ingredients[i].unit}</p>
+        </div>
+        `;
+      }
+    }
+    return container.innerHTML;
+  }
 }
