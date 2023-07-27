86fe751d9d3c236bbbe2c9b6c93ab4e02ebaabaa
beebrown <bruno.grosdesirs@outlook.fr>
2023-07-27 02:46:56 +0200

    maj

--- functions/ustensiles.js
+++ functions/ustensiles.js
@@ -63,18 +63,20 @@ export const searchUstensiles = (data) => {
 
   searchData.forEach(element => {
     element.addEventListener("click", () => {
+      const elementContent = element.innerText.replace(/\s/g, "");
       const content = `
-      <div class="filtre_element filtre_${element.innerText}">
+      <div class="filtre_element filtre_${elementContent}">
         <p class="filtre_element_text">${element.innerText}</p>
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" class="xmark close_${element.innerText}"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" class="xmark close_${elementContent}"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
       </div>
       `
-      if (!result.textContent.includes(element.innerText)) {
+      if (!result.textContent.includes(elementContent)) {
         result.innerHTML += content;
-        let removeUstensile = document.querySelector(`.close_${element.innerText}`);
+        const removeUstensile = document.querySelector(`.close_${elementContent}`);
         removeUstensile.addEventListener("click", () => {
-          let removeItem = document.querySelector(`.filtre_${element.innerText}`);
+          const removeItem = document.querySelector(`.filtre_${elementContent}`);
           result.removeChild(removeItem);
+          console.log(removeItem);
         })
       }
     })
